516eaaaadb6d96632ba5c2ed3e0b786b9bfef7b9
WibuVPN <wibuvpnstore@gmail.com>
2026-08-02 07:51:02 +0700

    fix: ensure uninstall.sh completely wipes all bot-related cronjobs and helper binaries

--- uninstall.sh
+++ uninstall.sh
@@ -12,7 +12,10 @@ rm -rf /usr/local/etc/xray /etc/haproxy /etc/wibutunnel /etc/xray /var/log/xray
 
 # Hapus file executable menu & daemon
 rm -rf /usr/local/sbin/algojo-* /usr/local/sbin/unlocker-wibu
-rm -rf /usr/local/bin/menu* /usr/local/bin/m-* /usr/local/bin/lock-user /usr/local/bin/unlock-user /usr/local/bin/bot-daemon /usr/local/bin/cek-trafik
+rm -rf /usr/local/bin/menu* /usr/local/bin/m-* /usr/local/bin/lock-user /usr/local/bin/unlock-user /usr/local/bin/bot-daemon /usr/local/bin/cek-trafik /usr/local/bin/watchdog.sh /usr/local/bin/renew-cert-wibu.sh /usr/local/bin/xp
+
+# Bersihkan Cron Jobs bawaan Wibu Tunneling
+crontab -l | grep -v 'watchdog.sh' | grep -v 'xp' | grep -v 'unlocker-wibu' | grep -v 'renew-cert-wibu.sh' | crontab -
 
 # Hapus service systemd
 rm -f /etc/systemd/system/xray.service /etc/systemd/system/wibutunnel-bot.service
